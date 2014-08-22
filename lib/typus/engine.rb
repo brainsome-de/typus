dbf24bacdc9bb6d1908ff9b2a1c6a67acbb4843b
Phillip Oertel <me@phillipoertel.com>
2014-08-22 15:26:29 +0200

    Add glyph icons to precompile. Not sure this will help.

--- lib/typus/engine.rb
+++ lib/typus/engine.rb
@@ -2,12 +2,16 @@ module Admin
   class Engine < Rails::Engine
 
     initializer :assets do |config|
-      extra_dirs = Dir.glob(root.join('vendor/assets/*'))
-      extra_dirs.each do |dir|
+
+      asset_libraries = Dir.glob(root.join('vendor/assets/*'))
+      asset_libraries.each do |dir|
         %w(img css js).each do |asset_type|
           Rails.application.config.assets.paths << File.join(dir, asset_type)
         end
       end
+
+      Rails.application.config.assets.precompile += ['glyphicons-halflings.png', 'glyphicons-halflings-white.png']
+
     end
   end
 end
