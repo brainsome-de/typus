5d5fc00de467d4aa9037f7691f4d89b661eec787
Phillip Oertel <me@phillipoertel.com>
2014-08-22 15:07:44 +0200

    Add load paths for the non-default asset locations of bootstrap, chosen, formalize and adapt. This should finally fix the assets for the Rails 4 asset pipeline.

--- lib/typus/engine.rb
+++ lib/typus/engine.rb
@@ -1,4 +1,13 @@
 module Admin
   class Engine < Rails::Engine
+
+    initializer :assets do |config|
+      extra_dirs = Dir.glob(root.join('vendor/assets/*'))
+      extra_dirs.each do |dir|
+        %w(img css js).each do |asset_type|
+          Rails.application.config.assets.paths << File.join(dir, asset_type)
+        end
+      end
+    end
   end
 end
