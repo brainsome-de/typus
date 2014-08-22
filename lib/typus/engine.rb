248279f18dafe175506a25091e0d06272b2ca63e
Phillip Oertel <me@phillipoertel.com>
2014-08-22 17:00:58 +0200

    Correct comment

--- lib/typus/engine.rb
+++ lib/typus/engine.rb
@@ -3,12 +3,12 @@ module Admin
 
     initializer :assets do |config|
 
-      # Add all "frontend libraries" (bootcamp etc.) to the load path for the asset pipeline. 
-      # This is necessary since typus has a nondefault directory structure in vendor/assets.
+      # Add all "frontend libraries" (bootstrap etc.) to the asset pipeline load path. 
+      # This is necessary because typus uses a nondefault directory structure in vendor/assets.
       extra_asset_paths = Dir.glob(File.join(root, 'vendor/assets/*/{img,images,css,js}'))
       Rails.application.config.assets.paths += extra_asset_paths
 
-      # Add nondefault image directory to the precompile array, otherwise images in there they won't
+      # Add nondefault image directory to the precompile array, otherwise images in there won't
       # become available for the asset pipeline.
       Rails.application.config.assets.precompile << %r{^img/.+}
 
