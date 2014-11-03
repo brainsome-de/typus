39a0d90b8932c34a5e954ad8ef7991c9a2b3fbe0
Michael Nowak <nowak@taktsoft.com>
2014-11-03 18:40:15 +0100

    moves routes mapping from routes.rb to engine.rb
    This was done to prevent Rails::Application::RoutesReloader#reload! from loading them twice.

--- lib/typus/engine.rb
+++ lib/typus/engine.rb
@@ -13,5 +13,55 @@ module Admin
       Rails.application.config.assets.precompile << %r{^img/.+}
       Rails.application.config.assets.precompile += %w( glyphicons-halflings.png glyphicons-halflings-white.png)
     end
+
+    # Moved from config/routes.rb to this initializer. This was done to prevent
+    # Rails::Application::RoutesReloader#reload! from loading them twice.
+    initializer :add_routing_paths do |app|
+      app.routes.append do
+        routes_block = lambda do
+
+          dashboard = Typus.subdomain ? "/dashboard" : "/admin/dashboard"
+
+          get "/" => redirect(dashboard)
+          get "dashboard" => "dashboard#index", :as => "dashboard_index"
+          get "dashboard/:application" => "dashboard#show", :as => "dashboard"
+
+          if Typus.authentication == :session
+            resource :session, :only => [:new, :create], :controller => :session do
+              delete :destroy, :as => "destroy"
+            end
+
+            resources :account, :only => [:new, :create, :show] do
+              collection do
+                get :forgot_password
+                post :send_password
+              end
+            end
+          end
+
+          Typus.models.map(&:to_resource).each do |_resource|
+            get "#{_resource}(/:action(/:id))(.:format)", :controller => _resource
+            post "#{_resource}(/:action(/:id))(.:format)", :controller => _resource
+            patch "#{_resource}(/:action(/:id))(.:format)", :controller => _resource
+            delete "#{_resource}(/:action(/:id))(.:format)", :controller => _resource
+          end
+
+          Typus.resources.map(&:underscore).each do |_resource|
+            get "#{_resource}(/:action(/:id))(.:format)", :controller => _resource
+            post "#{_resource}(/:action(/:id))(.:format)", :controller => _resource
+          end
+
+        end
+
+        if Typus.subdomain
+          constraints :subdomain => Typus.subdomain do
+            namespace :admin, :path => "", &routes_block
+          end
+        else
+          scope "admin", {:module => :admin, :as => "admin"}, &routes_block
+        end
+      end
+    end
+
   end
 end
