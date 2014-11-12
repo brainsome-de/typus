6ceee9f6cf11fc1178827dbaca592a70b8df4984
Michael Nowak <nowak@taktsoft.com>
2014-11-12 13:24:39 +0100

    fixes namespace for Typus module in moved routes

--- lib/typus/engine.rb
+++ lib/typus/engine.rb
@@ -20,13 +20,13 @@ module Admin
       app.routes.append do
         routes_block = lambda do
 
-          dashboard = Typus.subdomain ? "/dashboard" : "/admin/dashboard"
+          dashboard = ::Typus.subdomain ? "/dashboard" : "/admin/dashboard"
 
           get "/" => redirect(dashboard)
           get "dashboard" => "dashboard#index", :as => "dashboard_index"
           get "dashboard/:application" => "dashboard#show", :as => "dashboard"
 
-          if Typus.authentication == :session
+          if ::Typus.authentication == :session
             resource :session, :only => [:new, :create], :controller => :session do
               delete :destroy, :as => "destroy"
             end
@@ -39,22 +39,22 @@ module Admin
             end
           end
 
-          Typus.models.map(&:to_resource).each do |_resource|
+          ::Typus.models.map(&:to_resource).each do |_resource|
             get "#{_resource}(/:action(/:id))(.:format)", :controller => _resource
             post "#{_resource}(/:action(/:id))(.:format)", :controller => _resource
             patch "#{_resource}(/:action(/:id))(.:format)", :controller => _resource
             delete "#{_resource}(/:action(/:id))(.:format)", :controller => _resource
           end
 
-          Typus.resources.map(&:underscore).each do |_resource|
+          ::Typus.resources.map(&:underscore).each do |_resource|
             get "#{_resource}(/:action(/:id))(.:format)", :controller => _resource
             post "#{_resource}(/:action(/:id))(.:format)", :controller => _resource
           end
 
         end
 
-        if Typus.subdomain
-          constraints :subdomain => Typus.subdomain do
+        if ::Typus.subdomain
+          constraints :subdomain => ::Typus.subdomain do
             namespace :admin, :path => "", &routes_block
           end
         else
--- lib/typus/version.rb
+++ lib/typus/version.rb
@@ -3,7 +3,7 @@ module Typus
     MAJOR = 4
     MINOR = 0
     TINY  = 0
-    PRE   = "beta4"
+    PRE   = "beta5"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
